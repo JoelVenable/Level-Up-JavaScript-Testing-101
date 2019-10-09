6b4a89eba31734755144d2cd646cbab658fa78a4
Joel <joelmusicman@gmail.com>
2019-10-09 18:18:00 -0500

    Wrote test for add function

--- src/App.test.js
+++ src/App.test.js
@@ -1,3 +1,13 @@
-test("Fake test", () => {
-  expect(true).toEqual(true);
+import { add, multiply } from "./App";
+
+test("add", () => {
+  // arrange
+  const num1 = 32;
+  const num2 = 12;
+
+  // act
+  const value = add(num1, num2);
+
+  // assert
+  expect(value).toEqual(44);
 });

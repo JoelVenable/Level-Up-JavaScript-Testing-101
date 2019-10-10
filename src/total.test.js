80f994f3b5f0796c44a51314d7125d06f86fdef7
Joel <joelmusicman@gmail.com>
2019-10-09 19:22:45 -0500

    Finished mock with passing tests

--- src/total.test.js
+++ src/total.test.js
@@ -1,11 +1,17 @@
 import { total } from "./total";
 import { add } from "./add";
 
-jest.mock("./add");
+jest.mock("./add", () => ({
+  add: jest.fn(() => 55)
+}));
 
 test("total", () => {
   const result1 = total(5, 50);
+
+  add.mockImplementation(() => 17);
   const result2 = total(3, 14);
+
+  expect(add).toHaveBeenCalledTimes(2);
 
   expect(result1).toEqual("$55");
   expect(result2).toEqual("$17");
